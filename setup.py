72df63a8bd45430107a3362f92ba838371a5c9b1
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2022-05-02 00:59:45 -0400

    Commit

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         ],
-    version='0.2.0.2',
+    version='0.2.1',
 
     #######################################################################
     #FILES
