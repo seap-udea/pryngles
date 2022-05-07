71e3f4ca2db2add30eb919096392ef94fa81256a
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2022-05-07 19:47:09 -0400

    Commit

---
+++ dev/_template.py
@@ -0,0 +1,35 @@
+##################################################################
+#                                                                #
+#.#####...#####...##..##..##..##...####...##......######...####..#
+#.##..##..##..##...####...###.##..##......##......##......##.....#
+#.#####...#####.....##....##.###..##.###..##......####.....####..#
+#.##......##..##....##....##..##..##..##..##......##..........##.#
+#.##......##..##....##....##..##...####...######..######...####..#
+#................................................................#
+#                                                                #
+# PlanetaRY spanGLES                                             #
+# The bright-side of the light-curve of (ringed) exoplanets      #
+#                                                                #
+##################################################################
+# Jorge I. Zuluaga, Mario Sucerquia, Jaime A. Alvarado (C) 2022  #
+##################################################################
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Pryngles module
+
+# ## Module template
+
+class Example(object):
+    example=1
+
+# # Convert
+
+get_ipython().run_cell_magic('javascript', '', 'IPython.notebook.kernel.execute(\'FILE=\\"\' + IPython.notebook.notebook_name + \'\\"\')')
+
+get_ipython().system('make -C .. DEVFILES=dev/{FILE} convert')
+
+# Converted
+
+# --debugging--
+
