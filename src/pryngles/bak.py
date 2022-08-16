0b53174df1275928753c8a1fda6c6f4391a69e4d
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2022-08-15 21:12:23 -0500

    Commit

--- src/pryngles/bak.py
+++ src/pryngles/bak.py
@@ -35,24 +35,23 @@ import math
 
 # ## Constants
 
-""" Sampler presets are the values of N 
-    for which there are already stored samples
 """
-
-SAMPLER_SPHERE_PRESETS = np.array(
-    list(np.arange(100, 1000, 100))+\
-    list(np.arange(1000, 2000, 200))+\
-    list(np.arange(2000, 3000, 300))+\
-    list(np.arange(3000, 4000, 400))+\
-    list(np.arange(4000, 5000, 500))+\
+Sampler presets are the values of N for which there are already stored samples
+"""
+SAMPLER_SPHERE_PRESETS=np.array(
+    list(np.arange(100,1000,100))+\
+    list(np.arange(1000,2000,200))+\
+    list(np.arange(2000,3000,300))+\
+    list(np.arange(3000,4000,400))+\
+    list(np.arange(4000,5000,500))+\
     [5000]
 )
-SAMPLER_CIRCLE_PRESETS = np.arange(100, 6000, 100)
-SAMPLER_PRESETS = ["sphere", "circle", "ring"]
+SAMPLER_CIRCLE_PRESETS=np.arange(100,6000,100)
+SAMPLER_PRESETS=["sphere","circle","ring"]
 
 # ## Sampler class
 
-Sampler_doc = f"""    Fibonacci sampling of disks and spheres.
+Sampler_doc=f"""    Fibonacci sampling of disks and spheres.
     
     Initialization attributes:
         N: integer:
@@ -141,20 +140,20 @@ class Sampler(PrynglesCommon):
                 raise ValueError(f"No presets for {preset} available.  This are the available presets: {SAMPLER_PRESETS}.")
             
             #Modify N if it is a ring
-            qring = False
+            qring=False
             if preset is "ring":
-                N = N / (1-ri**2)
-                preset = "circle"
-                qring = True
+                N=N/(1-ri**2)
+                preset="circle"
+                qring=True
             
             #Calculate the closest Npreset
             exec(f"self.Npreset=SAMPLER_{preset.upper()}_PRESETS[abs({N}-SAMPLER_{preset.upper()}_PRESETS).argmin()]")
             
-            Npreset = self.Npreset
-            filename = Misc.get_data(f"sampler_presets/sample_{preset}_N_{Npreset}.pkl")
+            Npreset=self.Npreset
+            filename=Misc.get_data(f"sampler_presets/sample_{preset}_N_{Npreset}.pkl")
             self.load_from(filename)
-            self.Npreset = Npreset
-            self.filename = filename
+            self.Npreset=Npreset
+            self.filename=filename
             
             #If the preset is a ring cut a hole
             if qring:
@@ -162,21 +161,21 @@ class Sampler(PrynglesCommon):
             return
         
         #Basic
-        self.N = N
-        self.seed = seed
+        self.N=N
+        self.seed=seed
         
         #Derivative
-        self.dim = 0
-        self.ss = None
-        self.pp = None
-        self.dmin = self.dmed = self.dmax = self.dran = self.dstar = 0
+        self.dim=0
+        self.ss=None
+        self.pp=None
+        self.dmin=self.dmed=self.dmax=self.dran=self.dstar=0
         
         #Purge
-        self.purged = False
+        self.purged=False
         
         #Plotting
-        self.cargs = dict(color="k", fill=False, alpha=0.1) # 2d plot
-        self.wargs = dict(color="k", lw=0.1) # 3d plot
+        self.cargs=dict(color="k",fill=False,alpha=0.1) # 2d plot
+        self.wargs=dict(color="k",lw=0.1) # 3d plot
         
         #Random seed
         self._seed_sampler()
@@ -195,9 +194,9 @@ class Sampler(PrynglesCommon):
             dmin: float
                 minimum distance.
         """
-        deltas = rs - r
-        dist = np.einsum('ij,ij->i', deltas, deltas)
-        imin = np.argsort(dist)[1]
+        deltas=rs-r
+        dist=np.einsum('ij,ij->i',deltas,deltas)
+        imin=np.argsort(dist)[1]
         return np.sqrt(dist[imin])
 
     def _calc_distances(self):
@@ -207,72 +206,73 @@ class Sampler(PrynglesCommon):
         Update:
             ds,dmin,dmax,dmed,dran,dstar.
         """
-        self.ds = np.array([self._get_min_distance(self.ss[i],self.ss) for i in range(len(self.ss))])
-        self.dmin = self.ds.min()
-        self.dmax = self.ds.max()
-        self.dmed = np.median(self.ds)
-        self.dran = self.dmax-self.dmin
-        self.dstar = np.sqrt(self.N)*self.dmed
+        self.ds=np.array([self._get_min_distance(self.ss[i],self.ss) for i in range(len(self.ss))])
+        self.dmin=self.ds.min()
+        self.dmax=self.ds.max()
+        self.dmed=np.median(self.ds)
+        self.dran=self.dmax-self.dmin
+        self.dstar=np.sqrt(self.N)*self.dmed
         
         #Area of each spangle
-        self.aes = self.A/self.N
+        self.aes=self.A/self.N
 
         #Effective diameter
-        self.deff = 2*(self.aes/np.pi)**0.5
+        self.deff=2*(self.aes/np.pi)**0.5
 
     def _seed_sampler(self):
         if self.seed:
             np.random.seed(self.seed)
             
-Sampler.__doc__ = Sampler_doc
+Sampler.__doc__=Sampler_doc
 
-def plot(self, spangled=dict(), **args):
+def plot(self,spangled=dict(),**args):
     """
     Plot sample.
 
     Parameters:
         args: scatter plotting options, dictionary.
     """
-    sargs= dict(c='k', s=1.5)
+    sargs=dict(c='k',s=1.5)
     sargs.update(args)
     
     #Spangles options
     if spangled:
-        defaults = dict(alpha=0.3)
+        defaults=dict(alpha=0.3)
         defaults.update(spangled)
     
-    if self.dim == 2:            
-        self.fig,self.ax = plt.subplots()
-        self.ax.scatter(self.ss[:,0], self.ss[:,1], **sargs)
-        self.ax.add_patch( plt.Circle((0,0), 1, **self.cargs))
+    if self.dim==2:            
+        self.fig,self.ax=plt.subplots()
+        self.ax.scatter(self.ss[:,0],self.ss[:,1],**sargs)
+        self.ax.add_patch(plt.Circle((0,0),1,**self.cargs))
         self.ax.set_aspect("equal")
         if spangled:
             for isp in range(self.N):
-                c = Circle(self.ss[isp], self.deff/2, **defaults)
+                c=Circle(self.ss[isp],self.deff/2,**defaults)
                 self.ax.add_patch(c)
                 
     else:
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(111, projection='3d')
-        self.ax.scatter(self.ss[:,0], self.ss[:,1], self.ss[:,2], **sargs)
-        u,v = np.mgrid[0:2*np.pi:20j,0:np.pi:10j]
-        x = np.cos(u)*np.sin(v)
-        y = np.sin(u)*np.sin(v)
-        z = np.cos(v)
-        self.ax.plot_wireframe(x, y, z, **self.wargs)
+        self.fig=plt.figure()
+        self.ax=self.fig.add_subplot(111,projection='3d')
+        self.ax.scatter(self.ss[:,0],self.ss[:,1],self.ss[:,2],**sargs)
+        u,v=np.mgrid[0:2*np.pi:20j,0:np.pi:10j]
+        x=np.cos(u)*np.sin(v)
+        y=np.sin(u)*np.sin(v)
+        z=np.cos(v)
+        self.ax.plot_wireframe(x,y,z,**self.wargs)
         
         if spangled:
             for isp in range(self.N):
-                Plot.circle3d(self.ax, self.ss[isp], self.deff/2, zDir=self.ss[isp], **defaults)
+                Plot.circle3d(self.ax,self.ss[isp],self.deff/2,zDir=self.ss[isp],**defaults)
         
-        self.ax.set_box_aspect([1 ,1 , 1])
+        self.ax.set_box_aspect([1,1,1])
         
     self.fig.tight_layout()
 
-Sampler.plot = plot
+Sampler.plot=plot
 
-def gen_circle(self, perturbation=1, boundary=2):
-    """ Sample points in fibonacci spiral on the unit circle
+def gen_circle(self,perturbation=1,boundary=2):
+    """
+    Sample points in fibonacci spiral on the unit circle
 
     Optional parameters:
 
@@ -286,59 +286,60 @@ def gen_circle(self, perturbation=1, boundary=2):
     self._seed_sampler()
     
     #Unitary radius
-    self.R = 1
+    self.R=1
 
     #Total area
-    self.A = np.pi*self.R**2
+    self.A=np.pi*self.R**2
         
     shift = 1.0 if perturbation == 0 else self.N*np.random.random()
 
-    ga = np.pi * (3.0-np.sqrt(5.0))
+    ga = np.pi * (3.0 - np.sqrt(5.0))
 
     # Boundary points
-    np_boundary = round(boundary*np.sqrt(self.N))
+    np_boundary = round(boundary * np.sqrt(self.N))
 
-    self.dim = 2
+    self.dim=2
     self.ss = np.zeros((self.N,3))
     self.pp = np.zeros((self.N,3))
     j = 0
     for i in range(self.N):
-        if i > self.N - (np_boundary+1):
+        if i > self.N - (np_boundary + 1):
             r = 1.0
         else:
-            r = np.sqrt((i+0.5) / (self.N - 0.5 * (np_boundary+1)))
-        phi   = ga * (i+shift)
-        self.ss[j,:] = np.array([r*np.cos(phi), r*np.sin(phi), 0])
-        self.pp[j,:] = np.array([r, np.mod(phi, 2*np.pi), 0])
+            r = np.sqrt((i + 0.5) / (self.N - 0.5 * (np_boundary + 1)))
+        phi   = ga * (i + shift)
+        self.ss[j,:] = np.array([r * np.cos(phi), r * np.sin(phi),0])
+        self.pp[j,:] = np.array([r,np.mod(phi,2*np.pi),0])
         j += 1
 
     #Distances
     self._calc_distances()
     
-Sampler.gen_circle = gen_circle
+Sampler.gen_circle=gen_circle
 
 
-def _cut_hole(self, ri=0):
+def _cut_hole(self,ri=0):
     """
     Cut a hole in the data (this applies to circle sampling)
     """
     #Purge points
-    cond =~ ((self.pp[:,1]>0) & (self.pp[:,0]<=ri))
-    self.pp = self.pp[cond]
-    self.ss = self.ss[cond]
-    self.N = len(self.pp)
+    cond=~((self.pp[:,1]>0)&(self.pp[:,0]<=ri))
+    self.pp=self.pp[cond]
+    self.ss=self.ss[cond]
+    self.N=len(self.pp)
     
     #Correct area
-    self.A = self.A - np.pi*ri**2
+    self.A=self.A-np.pi*ri**2
     
     #Distances
     self._calc_distances()
     
-Sampler._cut_hole = _cut_hole
+Sampler._cut_hole=_cut_hole
 
 
-def gen_ring(self, ri=0.5, perturbation=1, boundary=2):
-    """ Sample points in fibonacci spiral on the unit circle, but including an inner gap (as in ring)
+def gen_ring(self,ri=0.5,perturbation=1,boundary=2):
+    """
+    Sample points in fibonacci spiral on the unit circle, but including an inner gap (as in ring)
 
     Parameters:
         fi: float, default = 0.5:
@@ -355,21 +356,22 @@ def gen_ring(self, ri=0.5, perturbation=1, boundary=2):
     Update:
         ss, pp
     """
-    if self.N < 100:
+    if self.N<100:
         raise ValueError(f"The number of points for a ring shouldn't be lower than 100.  You provided {self.N}")
 
     #Compute effective number
-    self.N = int(self.N / (1-ri**2))
+    self.N=int(self.N/(1-ri**2))
     self.gen_circle(perturbation,boundary)
     
     #Cut hole
     self._cut_hole(ri)
         
-Sampler.gen_ring = gen_ring
+Sampler.gen_ring=gen_ring
 
 
-def gen_sphere(self, perturbation=1):
-    """ Sample points in the unit sphere following fibonacci spiral
+def gen_sphere(self,perturbation=1):
+    """
+    Sample points in the unit sphere following fibonacci spiral
 
     Optional parameters:
         perturbation: type of perturbation (0 normal perturbation, 1 random perturbation), int
@@ -380,38 +382,38 @@ def gen_sphere(self, perturbation=1):
     self._seed_sampler()
 
     #Unitary radius
-    self.R = 1
+    self.R=1
 
     #Total area of the Sample
-    self.A = 4*np.pi*self.R**2
+    self.A=4*np.pi*self.R**2
 
-    shift = 1.0 if perturbation == 0 else self.N*np.random.random()
+    shift = 1.0 if perturbation == 0 else self.N * np.random.random()
 
-    ga = np.pi * (3.0-np.sqrt(5.0))
-    offset = 2.0/self.N
+    ga = np.pi * (3.0 - np.sqrt(5.0))
+    offset = 2.0 / self.N
 
-    self.dim = 3
+    self.dim=3
     self.ss = np.zeros((self.N,self.dim))
     self.pp = np.zeros((self.N,self.dim))
     j = 0
     for i in range(self.N):
-        phi   = ga * ((i+shift) % self.N)
+        phi   = ga * ((i + shift) % self.N)
         cos_phi = np.cos(phi)
         sin_phi = np.sin(phi)
-        cos_theta = ((i+0.5) * offset) - 1.0
+        cos_theta = ((i + 0.5) * offset) - 1.0
         sin_theta = np.sqrt(1.0 - cos_theta*cos_theta)
-        theta = np.arccos(cos_theta)            
-        self.ss[j,:] = np.array([cos_phi*sin_theta, sin_phi*sin_theta, cos_theta])
-        self.pp[j,:] = np.array([1, np.mod(phi,2*np.pi), np.pi/2 - theta])
+        theta=np.arccos(cos_theta)            
+        self.ss[j,:] = np.array([cos_phi * sin_theta, sin_phi * sin_theta, cos_theta])
+        self.pp[j,:] = np.array([1,np.mod(phi,2*np.pi),np.pi/2-theta])
         j += 1
 
     #Distances
     self._calc_distances()
 
-Sampler.gen_sphere = gen_sphere
+Sampler.gen_sphere=gen_sphere
 
 
-def purge_sample(self, tol=0.5):
+def purge_sample(self,tol=0.5):
     """
     Purge sample, ie. remove points close than a given threshold.
 
@@ -425,21 +427,21 @@ def purge_sample(self, tol=0.5):
         print("Already purged.")
         return 0
 
-    self.purged = False
-    purge = True
+    self.purged=False
+    purge=True
     while purge:
         self._calc_distances()
-        if self.dmin < tol*self.dmed:
-            ipurge = np.argsort(self.ds)[0]
-            self.ss = np.delete(self.ss,ipurge,0)
-            self.pp = np.delete(self.pp,ipurge,0)
-            self.N -= 1
-            self.purged = True
-            purge = True
+        if self.dmin<tol*self.dmed:
+            ipurge=np.argsort(self.ds)[0]
+            self.ss=np.delete(self.ss,ipurge,0)
+            self.pp=np.delete(self.pp,ipurge,0)
+            self.N-=1
+            self.purged=True
+            purge=True
         else:
-            purge = False
+            purge=False
 
-Sampler.purge_sample = purge_sample
+Sampler.purge_sample=purge_sample
 
 
 # ## Test presets
