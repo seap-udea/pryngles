c7c27c5ca99b8ac0d1efe40a61eea8c54ee43d36
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2022-11-07 21:47:07 +1100

    Added pywidgets as a requirement

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setuptools.setup(
     install_requires=[
         'rebound', 'scipy', 'ipython', 'matplotlib', 'tqdm',
         'dill', 'spiceypy', 'cmasher','pandas','celluloid',
-        'sigfig','anytree'
+        'sigfig','anytree', 'pywidgets'
     ],
 
     # ######################################################################
