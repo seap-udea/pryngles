763b76b3a4f108b25ae1e73b81a7c83b1d143d1c
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2022-11-23 12:17:27 -0500

    Commit

--- bin/release.sh
+++ bin/release.sh
@@ -88,7 +88,7 @@ echo "version='$version'" > src/$PACKNAME/version.py
 # Build package
 ##################################################################
 echo "Building packages..."
-python -m build
+$PYTHON -m build
 
 ##################################################################
 # Uploading the package
@@ -97,9 +97,9 @@ echo
 if [ $qtype -eq 0 ]
 then
     echo "Uploading to Test PyPI (use __token__ as username and pypi-<token> as password)..."
-    python -m twine upload --repository testpypi dist/* --verbose
+    $PYTHON -m twine upload --repository testpypi dist/* --verbose
 else
     echo "Uploading to PyPI (use your username and password)..."
-    python -m twine upload dist/* --verbose
+    $PYTHON -m twine upload dist/* --verbose
 fi
 
