d2f6ffc9ac77aa2b63a45fe97874fc682c2fc9f7
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2022-11-30 08:34:23 -0500

    Commit

--- dev/cpixx/main.c
+++ dev/cpixx/main.c
@@ -5,17 +5,38 @@
 //////////////////////////////////////////////////////////////
 int main(void)
 {
+  int i;
+
+  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // TEST BRACK
+  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  FILE *f=fopen("xmu.mat","r");
+  double xmu[21];
+
+  for(i=0;i<20;i++){
+    fscanf(f,"%lf",&xmu[i]);
+    printf("%.17lf\n",xmu[i]);
+  }
   //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   // TEST SPLINE
   //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  /*
   #define n 10
   double x[n],y[n],y2[n];
-  for(int i=0;i<n;i++){
+  for(i=0;i<n;i++){
     x[i]=i+1;
     y[i]=x[i]*x[i];
   }
   spline(x,y,n,y2);
-  for(int i=0;i<n;i++){
+  for(i=0;i<n;i++){
     printf("%d %.17lf %.17lf %.17lf\n",i,x[i],y[i],y2[i]);
   }
+  
+  double xv,yv;
+  for(i=1;i<n;i++){
+    xv=i+0.3;
+    yv=splint(x,y,y2,n,xv);
+    printf("%.17lf %.17lf\n",xv,yv);
+  }
+  */
 }
--- dev/cpixx/main.f
+++ dev/cpixx/main.f
@@ -5,59 +5,88 @@
       PROGRAM test
 
 *======================================================================
+*     Test bracks
+*======================================================================
+c$$$      DOUBLE PRECISION xmu(20)
+c$$$      DOUBLE PRECISION x
+c$$$      
+c$$$      OPEN(unit=1,file='xmu.mat',status='old',err=999)
+c$$$
+c$$$      DO i=1,20
+c$$$         READ(1,*) xmu(i)
+c$$$         write (*,*) xmu(i)
+c$$$      ENDDO
+c$$$
+c$$$      DO i=1,10
+c$$$         x=i/10
+c$$$      ENDDO
+c$$$      
+c$$$      STOP
+c$$$999   WRITE(*,*) 'Error opening file!'
+      
+*======================================================================
 *     Test spline
 *======================================================================
-      PARAMETER (n=10)
-      DOUBLE PRECISION x(n),y(n),y2(n)
-
-      write(*,*) "Test spline"
-      DO i=1,10
-         x(i)=i
-         y(i)=x(i)**2
-      ENDDO
-
-      CALL spline(x,y,n,y2)
-      
-      DO i=1,10
-         write(*,*) i,x(i),y(i),y2(i)
-      ENDDO
+c$$$      PARAMETER (n=10)
+c$$$      DOUBLE PRECISION x(n),y(n),y2(n)
+c$$$      DOUBLE PRECISION xv,yv
+c$$$      
+c$$$      write(*,*) "Test spline"
+c$$$      DO i=1,10
+c$$$         x(i)=i
+c$$$         y(i)=x(i)**2
+c$$$      ENDDO
+c$$$
+c$$$      CALL spline(x,y,n,y2)
+c$$$      
+c$$$      DO i=1,10
+c$$$         write(*,*) i,x(i),y(i),y2(i)
+c$$$      ENDDO
+c$$$
+c$$$      xv = 4.3
+c$$$
+c$$$      DO i=1,9
+c$$$         xv = i+3.D-1
+c$$$         CALL splint(x,y,y2,n,xv,yv)
+c$$$         write(*,*) xv,yv
+c$$$      ENDDO
       
 *======================================================================
 *     Test reflection
 *======================================================================
-c$$$      PARAMETER (NPIX_=1,NMUGS_=21,NMAT_=3,NFOU_=3)
-c$$$
-c$$$      DOUBLE PRECISION phi(NPIX_),beta(NPIX_),
-c$$$     .     theta0(NPIX_),theta(NPIX_),apix(NPIX_),
-c$$$     .     Sarr(NPIX_,NMAT_+1)
-c$$$
-c$$$      CHARACTER (len=100) :: filename
-c$$$
-c$$$      DOUBLE PRECISION xmu(NMUGS_),rfou(NMAT_*NMUGS_,NMUGS_,0:NFOU_)
-c$$$      
-c$$$      INTEGER :: npix=NPIX_,nmat=NMAT_,nmugs=NMUGS_,nfou=NFOU_
-c$$$
-c$$$      filename='fou_gasplanet.dat'
-c$$$
-c$$$      CALL rdfous_planet(filename,nfou,nmat,nmugs,xmu,rfou)
-c$$$
-c$$$      phi(1)=1.0448451569439827
-c$$$      beta(1)=3.069394277348945
-c$$$      theta0(1)=0.04990329026929557
-c$$$      theta(1)=0.02509670973070432
-c$$$      apix(1)=9.432328787795567e-05
-c$$$      
-c$$$      CALL reflection(
-c$$$     .     npix,phi,beta,theta0,theta,
-c$$$     .     nmugs,nmat,nfou,xmu,rfou,
-c$$$     .     apix,Sarr)
-c$$$
-c$$$      DO i=1,1
-c$$$         DO j=1,NMAT_+1
-c$$$            write(*,*) Sarr(i,j)
-c$$$         ENDDO
-c$$$      ENDDO
-c$$$
+      PARAMETER (NPIX_=1,NMUGS_=21,NMAT_=3,NFOU_=3)
+
+      DOUBLE PRECISION phi(NPIX_),beta(NPIX_),
+     .     theta0(NPIX_),theta(NPIX_),apix(NPIX_),
+     .     Sarr(NPIX_,NMAT_+1)
+
+      CHARACTER (len=100) :: filename
+
+      DOUBLE PRECISION xmu(NMUGS_),rfou(NMAT_*NMUGS_,NMUGS_,0:NFOU_)
+      
+      INTEGER :: npix=NPIX_,nmat=NMAT_,nmugs=NMUGS_,nfou=NFOU_
+
+      filename='fou_gasplanet.dat'
+
+      CALL rdfous_planet(filename,nfou,nmat,nmugs,xmu,rfou)
+
+      phi(1)=1.0448451569439827
+      beta(1)=3.069394277348945
+      theta0(1)=0.04990329026929557
+      theta(1)=0.02509670973070432
+      apix(1)=9.432328787795567e-05
+      
+      CALL reflection(
+     .     npix,phi,beta,theta0,theta,
+     .     nmugs,nmat,nfou,xmu,rfou,
+     .     apix,Sarr)
+
+      DO i=1,1
+         DO j=1,NMAT_+1
+            write(*,*) Sarr(i,j)
+         ENDDO
+      ENDDO
+
 c$$$* Expected: 4.59785742e-07, 2.25122997e-07, 1.83440080e-09, 4.89642131e-01
       
       END
